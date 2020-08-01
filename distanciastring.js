c4a39e991c77c6b7c07161e2c4c6fa4f92de1141
jose <josloe@gmail.com>
2020-08-02 00:20:50 +0200

    added distanciastring

--- distanciastring.js
+++ distanciastring.js
@@ -1,30 +1,47 @@
 /*
     PROGRAMA DONDE ENCUENTRA LA DISTANCIA ENTRE UN CARACTER DEL STRING
-    Y EL ELEMENTO DEL STRING
+    Y CADA ELEMENTO DEL STRING
 
 */
 
 
-show_distancia("helloworld", "l");
+let resultado = show_distancia("helloworld", "l");
+console.log(resultado);
 //[2, 1, 0, 0, 1, 2, 2, 1, 0, 1]
 
 function show_distancia(palabra, caracter)
 {
+    let resultados = [];
+    let distanciaActual = parseInt(-Number.MIN_VALUE);
 
-    let distancias = [];
-    let distanciaActual = -1;
-    
-    for(let i = 0; i < palabra.length; i++)
+    for (let i = 0; i < palabra.length; i++ )
     {
-        if (palabra[i] === caracter)
+        if (palabra[i].toString() == caracter.toString())
         {
             distanciaActual = i;
         }
 
-
+        resultados.push(i - distanciaActual);
     }
 
-    return distancias;
+    // console.log(resultados);
+    // [5, 6, 0, 0, 1, 2, 3, 4, 0, 1]
+
+    distanciaActual = Number.MAX_VALUE;
+    for (let i = palabra.length - 1; i >= 0; i--)
+    {
+        if (palabra[i].toString() == caracter.toString())
+        {
+            distanciaActual = i;
+        }
+
+        resultados[i] = Math.min(resultados[i], distanciaActual - i);
+    }
+
+    // console.log(resultados);
+    //[2, 1, 0, 0, 1, 2, 2, 1, 0, 1]
+
+    return resultados;
+
 
 }
-
