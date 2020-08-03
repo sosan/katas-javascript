b405f69d90e77a8a06af5e160ffb51be806a5852
jose <josloe@gmail.com>
2020-08-03 17:15:59 +0200

    added numero mayoritario

---
+++ numerosmayoritarios.js
@@ -0,0 +1,47 @@
+/* 
+PROGRAMA DONDE ENCUENTRA UN ELEMENTO QUE APARECEN MAS VECES EN UN ARRAY
+Y DEVUELVE EL NUMERO MAYORITARIO JUNTO CON EL NUMERO DE APARICIONES
+*/
+
+let resultado = show_mayoritarios([3, 5, 3, 1, 1, 2, 1, 5, 1, 5, 3]);
+console.log(resultado);
+//[1, 4]
+
+function show_mayoritarios(arrayNumeros)
+{
+    let resultados = [];
+    resultados.push(-1, -1);
+    let numeroAparicion = 0;
+    let indiceArrayNumeros = 1;
+
+    
+    arrayNumeros.sort();
+    console.log(arrayNumeros);
+    //[1, 1, 1, 1, 2, 3, 3, 3, 5, 5, 5]
+    for (let i = 0; i < arrayNumeros.length; i++)
+    {
+
+        if (arrayNumeros[i] === arrayNumeros[indiceArrayNumeros])
+        {
+            numeroAparicion++;
+        }
+        else
+        {
+            numeroAparicion = 1;
+            indiceArrayNumeros = i;
+
+        }
+
+        if (numeroAparicion > resultados[1])
+        {
+            resultados[0] = arrayNumeros[i];
+            resultados[1] = numeroAparicion;
+        }
+
+
+    }
+
+    return resultados;
+
+}
+
